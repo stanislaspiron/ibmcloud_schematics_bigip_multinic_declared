c3179c2b541f1659d1d3d065beeb112195d68ff9
stanislaspiron <39823762+stanislaspiron@users.noreply.github.com>
2021-01-11 16:41:11 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -8,25 +8,10 @@ locals {
 
 resource "random_uuid" "namer" {}
 
-// open up port security security group
-resource "ibm_is_security_group" "f5_open_sg" {
-  name           = "sg-${random_uuid.namer.result}"
-  vpc            = data.ibm_is_subnet.f5_management_subnet.vpc
-  resource_group = data.ibm_is_subnet.f5_management_subnet.resource_group
+resource "ibm_is_security_group" "f5_mgmt_sg" {
+  identifier = var.management_security_group
 }
 
-// allow all inbound
-resource "ibm_is_security_group_rule" "f5_allow_inbound" {
-  depends_on = [ibm_is_security_group.f5_open_sg]
-  group      = ibm_is_security_group.f5_open_sg.id
-  direction  = "inbound"
-  remote     = "0.0.0.0/0"
-}
-
-// all all outbound
-resource "ibm_is_security_group_rule" "f5_allow_outbound" {
-  depends_on = [ibm_is_security_group_rule.f5_allow_inbound]
-  group      = ibm_is_security_group.f5_open_sg.id
-  direction  = "outbound"
-  remote     = "0.0.0.0/0"
+resource "ibm_is_security_group" "f5_tmm_sg" {
+  identifier = var.tmm_security_group
 }
