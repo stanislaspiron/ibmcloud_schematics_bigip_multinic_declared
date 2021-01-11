c882f4307b9033d00896af1af9cc4f7cefb20181
stanislaspiron <39823762+stanislaspiron@users.noreply.github.com>
2021-01-11 17:35:13 +0100

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -128,7 +128,7 @@ resource "ibm_is_instance" "f5_ve_instance" {
   zone       = data.ibm_is_subnet.f5_management_subnet.zone
   keys       = [data.ibm_is_ssh_key.ssh_pub_key.id]
   user_data  = data.template_file.user_data.rendered
-  depends_on = [ibm_is_security_group_rule.f5_allow_outbound]
+  #depends_on = [ibm_is_security_group_rule.f5_allow_outbound]
   timeouts {
     create = "60m"
     delete = "120m"
