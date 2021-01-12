6e35de800ca2c33aa72dd4c4c230244590682b63
stanislaspiron <39823762+stanislaspiron@users.noreply.github.com>
2021-01-12 16:25:54 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -8,14 +8,14 @@ locals {
 
 resource "random_uuid" "namer" {}
 
-#resource "ibm_is_security_group" "f5_management_sg" {
-#  name           = var.management_security_group
-#  vpc            = data.ibm_is_subnet.f5_management_subnet.vpc
-#  resource_group = data.ibm_is_subnet.f5_management_subnet.resource_group
-#}
+resource "ibm_is_security_group" "f5_management_sg" {
+  name           = var.management_security_group
+  vpc            = data.ibm_is_subnet.f5_management_subnet.vpc
+  resource_group = data.ibm_is_subnet.f5_management_subnet.resource_group
+}
 
-#resource "ibm_is_security_group" "f5_tmm_sg" {
-#  name           = var.tmm_security_group
-#  vpc            = data.ibm_is_subnet.f5_management_subnet.vpc
-#  resource_group = data.ibm_is_subnet.f5_management_subnet.resource_group
-#}
+resource "ibm_is_security_group" "f5_tmm_sg" {
+  name           = var.tmm_security_group
+  vpc            = data.ibm_is_subnet.f5_management_subnet.vpc
+  resource_group = data.ibm_is_subnet.f5_management_subnet.resource_group
+}
